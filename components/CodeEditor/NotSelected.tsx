12aefe848c249d3e4668c8ddf19e749c8aaf2bff
SuryaVats <sunnyvats2002@gmail.com>
2023-12-01 13:56:50 +0530

    chore:QuickStart IDE, Advanced IDE cards with their details and Empty State(NotSelected.tsx)

--- components/CodeEditor/NotSelected.tsx
+++ components/CodeEditor/NotSelected.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from "react";
 const NotSelected = () => {
-    return (
-        <div>Not Selected Yet</div>
-    )
-}
+  return (
+    // <div className="w-full overflow-auto h-full flex justify-center text-5xl items-center  text-zinc-600 ">
+    //   Select a file to Display.
+    // </div>
+    <div className="w-full h-full bg-[rgb(31,41,55)]  flex flex-row justify-center  overflow-auto text-slate-300 ">
+      <div className=" flex flex-col items-center ">
+        <img
+          src="./Images/Emptystate.png"
+          alt="dadaf"
+          className="w-52 h-52 mb-2 mt-32"
+        />
+        <h1 className="text-5xl text-center pb-2">Dashboard!</h1>
+        <p className="p-2">
+          Get started by creating a new file or selecting one you already
+          created.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default NotSelected
+export default NotSelected;
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const HeroSection = () => {
@@ -18,7 +19,7 @@ const HeroSection = () => {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href="#"
+                href="#plans"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started
@@ -33,10 +34,270 @@ const HeroSection = () => {
           </div>
         </div>
         <div
-          className="absolute  inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-35rem)]"
+          className="absolute  inset-x-0 top-[calc(100%-63rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-76rem)]"
           aria-hidden="true"
         >
           <div className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#384d5f] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"></div>
+        </div>
+        <div id="plans" className="flex mt-36 mb-16 ">
+          <div className="w-full mr-8 max-w-sm p-4 border rounded-lg shadow sm:p-8 bg-gray-800 border-gray-700">
+            <h5 className="mb-4 text-3xl font-bold text-gray-200 ">
+              QuickStart IDE:
+            </h5>
+            <p className=" text-gray-200 text-lg font-semibold">
+              Instant Coding without an account for Basic Tasks
+            </p>
+            <ul role="list" className="space-y-5 my-7">
+              <li className="flex items-center">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-blue-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-300 ms-3">
+                  No User Account Required
+                </span>
+              </li>
+              <li className="flex items-center">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-blue-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-300 ms-3">
+                  Online IDE (for Python, C++, JavaScript)
+                </span>
+              </li>
+              <li className="flex items-center">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-blue-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-300 ms-3">
+                  Acces to a Single File
+                </span>
+              </li>
+              <li className="flex items-center">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-blue-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-300 ms-3">
+                  Code Execution
+                </span>
+              </li>
+              <li className="flex line-through decoration-gray-500">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-500 ms-3">
+                  Storage
+                </span>
+              </li>
+              <li className="flex line-through decoration-gray-500">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-500 ms-3">
+                  Access files from anywhere
+                </span>
+              </li>
+              <li className="flex line-through decoration-gray-500">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-500 ms-3">
+                  Download Files
+                </span>
+              </li>
+              <li className="flex line-through decoration-gray-500">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-500 ms-3">
+                  Create, save and manage multiple files
+                </span>
+              </li>
+            </ul>
+            <button
+              type="button"
+              className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+            >
+              Continue without Login
+            </button>
+          </div>
+
+          <div className="w-full mr-8 max-w-sm p-4 border rounded-lg shadow sm:p-8 bg-gray-800 border-gray-700">
+            <h5 className="mb-4 text-3xl font-bold text-gray-200 ">
+              Advanced IDE:
+            </h5>
+            <p className="text-gray-200 text-lg font-semibold">
+              Unleash Your Creativity with Personalized Files and Collaboration.
+            </p>
+
+            <ul role="list" className="space-y-5 my-7">
+              <li className="flex items-center">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-blue-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-300 ms-3">
+                  User Account is required
+                </span>
+              </li>
+              <li className="flex items-center">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-blue-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-300 ms-3">
+                  Online IDE (for Python, C++, JavaScript)
+                </span>
+              </li>
+              <li className="flex items-center">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-blue-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-300 ms-3">
+                  Access to Multiple files
+                </span>
+              </li>
+              <li className="flex items-center">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-blue-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-300 ms-3">
+                  Code Execution
+                </span>
+              </li>
+              <li className="flex items-center">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-blue-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-300 ms-3">
+                  Storage
+                </span>
+              </li>
+              <li className="flex items-center">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-blue-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-300 ms-3">
+                  Access files from anywhere
+                </span>
+              </li>
+              <li className="flex items-center">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-blue-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-300 ms-3">
+                  Download Files
+                </span>
+              </li>
+              <li className="flex items-center">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-blue-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-300 ms-3">
+                  Create, save and manage multiple files
+                </span>
+              </li>
+            </ul>
+            <Link
+              href="/auth/login"
+              className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+            >
+              Login
+            </Link>
+          </div>
         </div>
       </div>
     </div>
